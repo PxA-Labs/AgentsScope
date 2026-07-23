c2137c9297289ea2290830e82eac4dbe8e6f570a
Archit Mittal <work.architmittal@gmail.com>
2026-07-23 15:32:24 +0530

    ci: add packages/server to sys.path in conftest.py

--- packages/server/tests/conftest.py
+++ packages/server/tests/conftest.py
@@ -1,5 +1,9 @@
-import pytest_asyncio
+import sys
 import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+import pytest_asyncio
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 
 import models
