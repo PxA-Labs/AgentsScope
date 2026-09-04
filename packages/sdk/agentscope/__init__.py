d9e60dc99d5f230c4f3a51f36082ce749e284c7c
SriRamkunamsetty <mohansriramkunamsetty@gmail.com>
2026-09-04 09:38:52 +0530

    feat(sdk): implement raw OpenAI API telemetry wrapper

--- packages/sdk/agentscope/__init__.py
+++ packages/sdk/agentscope/__init__.py
@@ -3,5 +3,14 @@
 from agentscope.callback import AgentScopeCallback
 from agentscope.decorators import trace
 from agentscope.exceptions import AgentScopeError, BudgetExceededError
+from agentscope.patch import patch_openai, unpatch_openai
 
-__all__ = ["AgentScopeCallback", "trace", "AgentScopeError", "BudgetExceededError"]
+__all__ = [
+    "AgentScopeCallback",
+    "trace",
+    "patch_openai",
+    "unpatch_openai",
+    "AgentScopeError",
+    "BudgetExceededError",
+]
+
